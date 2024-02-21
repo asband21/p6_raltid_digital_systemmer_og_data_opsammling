a83499a86ff370bda7c4baccefb351e213bfb824
asbjørn <qw41ha@student.aau.dk>
2024-02-21 16:27:32 +0100

    kvadraturinkoder kode er klar

---
+++ lek_4/kvadraturenkoder/kvadraturenkoder.ino
@@ -0,0 +1,33 @@
+int p1 = 12;
+int p2 = 3;
+
+//int re_pos = 10000;
+int re_pos = 0;
+
+//char index = 0;
+byte index = 0;
+int posison = 0;
+int record[16] = {0, 1, -1 ,re_pos, -1, 0, re_pos, 1, 1, re_pos, 0, -1, re_pos, +1, -1, 0};
+
+void setup()
+{
+	Serial.begin(115200);
+	pinMode(p1, INPUT);
+	pinMode(p2, INPUT);
+	Serial.println("hej");
+}
+
+void loop()
+{
+	index = index << 1;
+	index += digitalRead(p1);  
+	index = index << 1;
+	index += digitalRead(p2);
+	index = index & B00001111; 
+	posison += record[index];
+  /*if(index != 0)
+    Serial.println(index);
+	*/
+	if(record[index]*record[index])
+		Serial.println(posison);
+}
