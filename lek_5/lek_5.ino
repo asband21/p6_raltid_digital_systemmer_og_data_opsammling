66fa513cb1b3e5d3ff33f77b67068ab5cda0d195
asbjørn <qw41ha@student.aau.dk>
2024-02-23 11:44:51 +0100

    lek 5.3 kode

---
+++ lek_5/lek_5.ino
@@ -0,0 +1,31 @@
+const byte ledPin = LED_BUILTIN;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+volatile unsigned long tid = 0;
+volatile unsigned long teller = 0;
+void setup()
+{
+	pinMode(ledPin, OUTPUT);
+	pinMode(interruptPin, INPUT_PULLUP);
+	Serial.begin(115200);
+	attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
+}
+
+void loop()
+{
+	digitalWrite(ledPin, state);
+	Serial.println(teller);
+	delay(5);
+}
+
+void blink()
+{
+	unsigned long m_mid = millis();
+	if(m_mid - tid > 30)
+	{
+		state = !state;
+		teller++;
+		tid = m_mid;
+	}
+
+}
